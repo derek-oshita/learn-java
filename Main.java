c176686e5bfb88921d4617a3d345fe9e3cb0ca02
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2021-04-26 14:12:18 -0700

    V1 of mortgage calculator working.

--- Main.java
+++ Main.java
@@ -1,48 +1,35 @@
-import java.util.Date;
-
-
-import java.awt.Point;
+import java.text.NumberFormat;
+import java.util.*;
 
 // https://www.youtube.com/watch?v=eIrMbAQSU34&ab_channel=ProgrammingwithMosh
-// 43:00 mins
+// START PROJECT SECTION 
 
 public class Main {
 
-    public static class Drink {
-        private enum Size {
-            SMALL, MEDIUM, LARGE
-        }
-
-        public enum OrderName {
-            OLD_FASHIONED, MOJITO, AMF
-        }
-
-        Size size; 
-        OrderName orderName; 
-        String note; 
-    }
-
-
-    public static class Person {
-        String name; 
-        byte age; 
-        String location; 
-    }
-
     public static void main (String[] args) {
-        Drink drink_order_one = new Drink(); 
-        drink_order_one.size = Drink.Size.LARGE; 
-        drink_order_one.orderName = Drink.OrderName.OLD_FASHIONED; 
-        drink_order_one.note = "WHISKY IS SUPER SICK!"; 
-        
-        Person person_one = new Person(); 
-        person_one.name = "Derek"; 
-        person_one.age = 29; 
-        person_one.location = "San Francisco, CA"; 
+        Scanner scanner = new Scanner(System.in); 
 
-        System.out.println(person_one.name + " ordered a " + drink_order_one.size + " " + drink_order_one.orderName + "!" );
+        System.out.println("Principal Amount: ");
+        int principal = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Annual Interest Rate: ");
+        float annualInterestRate = (Float.parseFloat(scanner.nextLine()) / 100) / 12; 
+
+        System.out.println("Period (years): ");
+        int period = Integer.parseInt(scanner.nextLine()) * 12; 
+
+        double topHalf = Math.pow((1 + annualInterestRate), period) * annualInterestRate; 
+        double bottomHalf = Math.pow(1 + annualInterestRate, period) - 1; 
+
+        final double MORTGAGETOTAL = principal * (topHalf / bottomHalf); 
+
+        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(); 
+        System.out.println("Mortgage: " + currencyFormatter.format(MORTGAGETOTAL)); 
 
     }
+
 
 }
+
+
 
