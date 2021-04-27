463212bcc75872ca81b2b6d24a37107e17faf541
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2021-04-26 17:03:21 -0700

    Cleaned up the code.

--- Main.java
+++ Main.java
@@ -58,31 +58,7 @@ public class Main {
     }
 
 
-    // public class Mortgage {
-    //     Scanner scanner = new Scanner(System.in); 
 
-    //     final byte MONTHS_IN_A_YEAR = 12; 
-    //     final byte PERCENTAGE = 100; 
-
-    //     System.out.println("Principal Amount:");
-    //     int principal = scanner.nextInt(); 
-
-    //     System.out.println("Annual Interest Rate:");
-    //     float annualInterestRate = scanner.nextFloat(); 
-    //     float monthlyInterestRate = annualInterestRate / PERCENTAGE / MONTHS_IN_A_YEAR; 
-
-    //     System.out.println("Period (years):");
-    //     byte years = scanner.nextByte(); 
-    //     int numberOfPayments = years * MONTHS_IN_A_YEAR; 
-
-    //     double mortgage = principal
-    //                 * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments)) 
-    //                 / (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1); 
-
-    //     String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage); 
-
-    //     System.out.println("Mortgage: " + mortgageFormatted);
-    // }
 
 
 }
